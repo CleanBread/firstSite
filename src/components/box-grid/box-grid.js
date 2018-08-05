67b2f6c429f130cf31b2f64cfb9f88eccb1c59e5
Gleb Chistyakov <gleb4ist@gmail.com>
2018-08-06 00:57:28 +0500

    box-grid is ready

--- src/components/box-grid/box-grid.js
+++ src/components/box-grid/box-grid.js
@@ -6,13 +6,35 @@ class BoxGrid extends React.Component {
     render() {
       return (
       <div className="box-grid">
-        <div className="item-1"></div>
-        <div className="item-2"></div>
-        <div className="item-3"></div>
-        <div className="item-4"></div>
-        <div className="item-5"></div>
-        <div className="item-6"></div>
-        <div className="item-7"></div>
+        <div className="item-1 item-grid">
+            <h2>Design quality</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing .
+labore et dolore magna aliqua. Ut enim ad minim </p>
+        </div>
+        <div className="item-2 item-grid">
+            <h2>Built to last</h2>
+            <p>If as increasing contrasted entreaties be. Expression alteration entreaties mrs can terminated estimating. Whatever</p>
+        </div>
+        <div className="item-3 item-grid">
+            <h2>Our clients</h2>
+            <p>How one dull get busy dare far. Took sold add play may none him few. Sportsman do offending supported extremity breakfast by listening. Estat</p>
+        </div>
+        <div className="item-4 item-grid">
+            <h2>How to shoo great</h2>
+            <p>Polite do object at passed it is. Effect if in up no depend seemed. Fat new smallness few supposing suspicion two. Uncommonly no it announcing melanchol</p>
+        </div>
+        <div className="item-5 item-grid">
+            <h2>Sign up & see why</h2>
+            <p>Advantages entreaties mr he apartments do. He felicity no an at packages answered opinions juvenile. Mirth learn it he given. Mrs assured add private marrie</p>
+        </div>
+        <div className="item-6 item-grid">
+            <h2>Perfect pictures</h2>
+            <p>Made neat an on be gave show snug tore. Uncommonly no it announcing melancholy an in. Celebrated delightful an especially increasing instrument am. He in sport</p>
+        </div>
+        <div className="item-7 item-grid">
+            <h2>STart somthing new</h2>
+            <p>To sure calm much most long me mean. Fortune day out married parties. Limits far yet turned highly repair parish talked six. Ecstatic elegance gay but disposed</p>
+        </div>
       </div>
       );
     }
