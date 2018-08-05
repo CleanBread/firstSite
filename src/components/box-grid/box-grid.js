532fe0c99c0dc9a2a3b5f60f43c98e77009224a2
Gleb Chistyakov <gleb4ist@gmail.com>
2018-08-05 23:36:26 +0500

    header и блок с текстом готовы

--- src/components/box-grid/box-grid.js
+++ src/components/box-grid/box-grid.js
@@ -6,6 +6,13 @@ class BoxGrid extends React.Component {
     render() {
       return (
       <div className="box-grid">
+        <div className="item-1"></div>
+        <div className="item-2"></div>
+        <div className="item-3"></div>
+        <div className="item-4"></div>
+        <div className="item-5"></div>
+        <div className="item-6"></div>
+        <div className="item-7"></div>
       </div>
       );
     }
--- src/components/text/text.js
+++ src/components/text/text.js
@@ -6,12 +6,12 @@ class Text extends React.Component {
     render() {
       return (
       <div className="text">
-        {/* <h1 className="h1">Start something new</h1>
-        <p className="text-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <p className="text-2">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-        <p className="text-3">Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.</p>
-        <p className="text-4">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-        <p className="text-5">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p> */}
+        <div className="h1">Start something new</div>
+        <div className="item-1 filler">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        <div className="item-2 filler">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</div>
+        <div className="item-3 filler">Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.</div>
+        <div className="item-4 filler">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</div>
+        <div className="item-5 filler">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</div>
       </div>
       );
     }
