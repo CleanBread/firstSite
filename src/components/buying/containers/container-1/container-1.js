09017d927644cdd7cf58e3ee42c90b5baa8d3950
Gleb Chistyakov <gleb4ist@gmail.com>
2018-08-25 17:34:32 +0500

    containers is ready

--- src/components/buying/containers/container-1/container-1.js
+++ src/components/buying/containers/container-1/container-1.js
@@ -5,7 +5,22 @@ import './container-1.css'
 class Container_1 extends React.Component {
     render() {
       return (
-      <div className="container">
+      <div className="container-1">
+        <div className="legacy">
+            <span>LEGACY</span>
+        </div>
+        <div className="text-in-co_1">
+            <span>Go it solo</span>
+            <p>Any delicate you how kindness horrible outlived servants. If in so bred at dare rose lose good. Took sold add play may none him few. Celebrated delightful an especially increasing instrument am. Painful so he an comfort is manners. Feel and make two real miss use e</p>
+        </div>
+        
+        <div>
+          <button className="buying-btn" src="#">BUY NOW $9</button>
+        </div>
+        <div>
+          <a href="https://www.apple.com/" target="_blank"><div className="logo-apple"/></a>
+          <a href="http://www.microsoft.com/ru-ru/windows" target="_blank"><div className="logo-windows"/></a>
+        </div>
       </div>
       );
     }
--- src/components/buying/containers/container-2/container-2.js
+++ src/components/buying/containers/container-2/container-2.js
@@ -5,7 +5,22 @@ import './container-2.css'
 class Container_2 extends React.Component {
     render() {
       return (
-      <div className="container">
+      <div className="container-2">
+      <div className="most">
+            <span>Most Popular</span>
+        </div>
+        <div className="text-in-co_1">
+            <span>Team Plan</span>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu t am. Painful so he an comfort is manners. Feel and make two real miss use e</p>
+        </div>
+        
+        <div>
+          <button className="buying-btn" src="#">BUY NOW $18</button>
+        </div>
+        <div>
+          <a href="https://www.apple.com/" target="_blank"><div className="logo-apple"/></a>
+          <a href="http://www.microsoft.com/ru-ru/windows" target="_blank"><div className="logo-windows"/></a>
+        </div>
       </div>
       );
     }
